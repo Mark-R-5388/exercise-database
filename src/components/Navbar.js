5a14baf59e339a9e18b7e3d0a4cbff7d5f55430e
Mark Rose <markrose@markwebdev.com>
2022-01-03 10:09:01 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <h3 className="navbar-logo">Exercise Database</h3>
+      <ul>
+        <li>About</li>
+        <li>Exercises</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
